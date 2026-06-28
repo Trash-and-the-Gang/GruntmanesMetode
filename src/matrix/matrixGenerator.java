e227ce25df879c493bdc1658fb8d53dd943ba4bd
Rodrigo Sāmietis <samietisrodrigo@gmail.com>
2026-06-28 22:44:36 +0300

    Real changes

---
+++ src/matrix/matrixGenerator.java
@@ -0,0 +1,20 @@
+package matrix;
+
+import java.util.Random;
+
+public class matrixGenerator {
+	
+	public float[][] generateMatrix(int width, int height, float numberInterval){
+		
+		float[][] matrix = new float[width][height];
+		Random rand = new Random();
+		
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j < height; j++) {
+
+			}
+		}
+		
+		return new float[1][1];
+	}
+}
